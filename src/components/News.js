9b02fec8c9ccf412aef18571bedf603b7904f831
Vamsi7474 <105474329+Vamsi7474@users.noreply.github.com>
2024-03-21 23:00:25 +0530

    initial commit

--- src/components/News.js
+++ src/components/News.js
@@ -38,27 +38,26 @@ export class News extends Component {
 
   async UpdateNews() {
     this.props.setProgress(10);
-    let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d0020467e557421c810e62062b7d30fc&page=1&pageSize=${this.props.pageSize}`;
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=d0020467e557421c810e62062b7d30fc&page=1&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
-    let headers={
-      'Authorization':'Bearer d0020467e557421c810e62062b7d30fc' 
-    };
-    let data = await fetch(proxyUrl+url,{
-      method:'GET',
-      headers:headers
-    });
-    this.props.setProgress(30);
-    let parsedata = await data.json();
-    this.props.setProgress(30);
-    this.setState({
-      articles: parsedata.articles,
-      totalResults: parsedata.totalResults,
-      loading: false
-    });
-    this.props.setProgress(100);
-  }
-
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error('Failed to fetch news');
+      }
+      const data = await response.json();
+      this.props.setProgress(30);
+      this.setState({
+        articles: data.articles,
+        totalResults: data.totalResults,
+        loading: false
+      });
+      this.props.setProgress(100);
+    } catch (error) {
+      console.error('Error fetching news:', error);
+      this.setState({ loading: false });
+    }
+  }  
   HandlePrePage = async () => {
     this.setState({ page: this.state.page - 1 });
     this.UpdateNews();
